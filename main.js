7c00016f902f4fe3cf146563a40de3e6e47b18bf
Aya Osama <ayaramadan2011@gmail.com>
2024-11-26 04:48:07 +0200

    handle prev&next btns

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let data = {
+  img1: "./imgs/img1.png",
+  img2: "./imgs/img2.png",
+  img3: "./imgs/img3.png",
+  img4: "./imgs/img4.png",
 };
 
 //ul def
 let theUlid = document.getElementById("ull");
+
 // full ul with li
 let theUlCreated = document.createElement("ul");
 theUlid.appendChild(theUlCreated);
@@ -15,22 +20,53 @@ for (let i = 1; i <= 4; i++) {
 }
 
 //div id="div4" def
-let theDiv4 = document.getElementById("div4");  
+let theDiv4 = document.getElementById("div4");
+
 // prev btn
 let prev = document.createElement("span");
-prev.innerHTML = '&#8249;';
+prev.innerHTML = "&#8249;";
 theDiv4.appendChild(prev);
 
-// img 
-let theImg = document.createElement("img");
-theImg.src = "./imgs/img1.png";
-theDiv4.appendChild(theImg);
+// img
+let firstImg = document.createElement("img");
+firstImg.setAttribute("src", data["img1"]);
+let imgSrc = firstImg.getAttribute("src");
+theDiv4.appendChild(firstImg);
+console.log(imgSrc);
 
 // next btn
 let next = document.createElement("span");
 
-next.innerHTML = '&#8250;';
+next.innerHTML = "&#8250;";
 theDiv4.appendChild(next);
 console.log(theDiv4);
 
+let theImg = document.querySelector("#div4 img");
+function prevClick() {
+  if (imgSrc === data["img1"]) {
+    theImg.setAttribute("src", data["img4"]);
+  } else if (imgSrc === data["img2"]) {
+    theImg.setAttribute("src", data["img1"]);
+  } else if (imgSrc === data["img3"]) {
+    theImg.setAttribute("src", data["img2"]);
+  } else if (imgSrc === data["img4"]) {
+    theImg.setAttribute("src", data["img3"]);
+  }
+  imgSrc = theImg.getAttribute("src");
+}
 
+function nextClick() {
+  if (imgSrc === data["img1"]) {
+    theImg.setAttribute("src", data["img2"]);
+  } else if (imgSrc === data["img2"]) {
+    theImg.setAttribute("src", data["img3"]);
+  } else if (imgSrc === data["img3"]) {
+    theImg.setAttribute("src", data["img4"]);
+  } else if (imgSrc === data["img4"]) {
+    theImg.setAttribute("src", data["img1"]);
+  }
+  imgSrc = theImg.getAttribute("src");
+}
+
+prev.addEventListener("click", prevClick);
+next.addEventListener("click", nextClick);
