5fea5dbd078d25a8c15b22333c8c3081bccb3af9
Aya Osama <ayaramadan2011@gmail.com>
2024-11-26 06:04:44 +0200

    convert html to js

--- main.js
+++ main.js
@@ -5,6 +5,56 @@ let data = {
   img4: "./imgs/img4.png",
 };
 
+let title = {
+  title1: "untold",
+  title2: "story of",
+  title3: "KLCC",
+  title4: "Malaysia",
+};
+
+let contener = document.getElementById("d");
+let div11 = document.createElement("div");
+let div1 = document.createElement("div");
+div1.setAttribute("id", "div1");
+for (let i = 1; i <= 4; i++) {
+  let p = document.createElement("p");
+  p.setAttribute("id", `p${i}`);
+  p.textContent = title[`title${i}`];
+  div1.appendChild(p);
+}
+div11.appendChild(div1);
+
+// contener.appendChild(div11);
+let div12 = document.createElement("div");
+div12.setAttribute("id", "div2");
+
+let div22 = document.createElement("div");
+div22.setAttribute("id", "div22");
+
+let divpp = document.createElement("p");
+divpp.setAttribute("id", "pp");
+divpp.textContent =
+  "We so opinion friends me message as delight. Whole front do ofplate heard oh ought. His defective nor convinced residence own.";
+div22.appendChild(divpp);
+div12.appendChild(div22);
+
+let div3 = document.createElement("div");
+div3.setAttribute("id", "div3");
+let divp = document.createElement("p");
+divp.setAttribute("id", "p");
+divp.textContent = "Read more";
+div3.appendChild(divp);
+let imgReadmore = document.createElement("img");
+imgReadmore.setAttribute("src", "./imgs/Vector 1.png");
+div3.appendChild(imgReadmore);
+div12.appendChild(div3);
+div11.appendChild(div12);
+contener.appendChild(div11);
+let theDiv4 = document.createElement("div");
+theDiv4.setAttribute("id", "div4");
+
+contener.appendChild(theDiv4);
+
 //ul def
 let theUlid = document.getElementById("ull");
 
@@ -18,9 +68,6 @@ for (let i = 1; i <= 4; i++) {
   theLiCreated.textContent = i;
   theUlCreated.appendChild(theLiCreated);
 }
-
-//div id="div4" def
-let theDiv4 = document.getElementById("div4");
 
 // prev btn
 let prev = document.createElement("span");
@@ -36,13 +83,13 @@ console.log(imgSrc);
 
 // next btn
 let next = document.createElement("span");
-
 next.innerHTML = "&#8250;";
 theDiv4.appendChild(next);
 console.log(theDiv4);
 
+//prev&next click events
 let theImg = document.querySelector("#div4 img");
-theImg.style="hover:`clearInterval()`";
+
 function prevClick() {
   if (imgSrc === data["img1"]) {
     theImg.setAttribute("src", data["img4"]);
@@ -72,14 +119,17 @@ function nextClick() {
 prev.addEventListener("click", prevClick);
 next.addEventListener("click", nextClick);
 
+// auto slide
 let intervalId = setInterval(() => {
   nextClick();
 }, 3000);
 
+// remove auto slide on hover
 theImg.addEventListener("mouseover", () => {
   clearInterval(intervalId);
 });
 
+// add auto slide on mouseout
 theImg.addEventListener("mouseout", () => {
   intervalId = setInterval(nextClick, 5000);
 });
